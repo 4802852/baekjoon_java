be06be7fa8056aa7321778cc3b53b80a4b52e08d
4802852 <4802852@gmail.com>
2022-07-02 00:49:19 +0900

    boj 16639 complete

---
+++ danimartinwife/boj16639/Main.java
@@ -0,0 +1,65 @@
+package danimartinwife.boj16639;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+  static int N, ans;
+  static char[] input;
+  static int[][] dp_min, dp_max;
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(
+      new FileReader(
+        "/Users/hs/vscode/algorithm/algorithm/src/danimartinwife/boj16639/input.txt"
+      )
+    );
+    // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    N = Integer.parseInt(br.readLine());
+    input = br.readLine().toCharArray();
+    br.close();
+
+    dp_min = new int[N][N];
+    dp_max = new int[N][N];
+    for (int i = 0; i < N; i++) {
+      Arrays.fill(dp_min[i], Integer.MAX_VALUE);
+      Arrays.fill(dp_max[i], Integer.MIN_VALUE);
+    }
+    for (int i = 0; i < N; i += 2) {
+      dp_max[i][i] = input[i] - '0';
+      dp_min[i][i] = input[i] - '0';
+    }
+
+    for (int j = 2; j < N; j += 2) {
+      for (int i = 0; i < N - j; i += 2) {
+        for (int k = 2; k <= j; k += 2) {
+          char op = input[i + k - 1];
+          int[] tmp = new int[4];
+          tmp[0] = cal(dp_max[i][i + k - 2], dp_max[i + k][i + j], op);
+          tmp[1] = cal(dp_max[i][i + k - 2], dp_min[i + k][i + j], op);
+          tmp[2] = cal(dp_min[i][i + k - 2], dp_max[i + k][i + j], op);
+          tmp[3] = cal(dp_min[i][i + k - 2], dp_min[i + k][i + j], op);
+          Arrays.sort(tmp);
+          dp_max[i][i + j] = Math.max(dp_max[i][i + j], tmp[3]);
+          dp_min[i][i + j] = Math.min(dp_min[i][i + j], tmp[0]);
+        }
+      }
+    }
+
+    System.out.println(dp_max[0][N - 1]);
+  }
+
+  static int cal(int a, int b, char operator) {
+    if (operator == '+') {
+      return a + b;
+    } else if (operator == '-') {
+      return a - b;
+    } else {
+      return a * b;
+    }
+  }
+}
--- danimartinwife/boj18500/Main.java
+++ danimartinwife/boj18500/Main.java
@@ -46,10 +46,13 @@ public class Main {
     st = new StringTokenizer(br.readLine());
     for (int t = 0; t < N; t++) {
       int h = R - Integer.parseInt(st.nextToken());
+      // 높이 h에서 창영이 던졌으면 chang = 1, 그렇지 않다면 chang = -1
       throwStone(h, chang);
       chang *= -1;
+      // 각 미네랄 덩이를 BFS 로 탐색하되, 공중에 떠있는 미네랄 덩이를 찾는다.
       int flyNum = flyCheckBFS();
       if (flyNum == 0) continue;
+      // 공중에 떠있는 미네랄 덩이를 움직일 수 있는 만큼 아래로 이동시킨다.
       fall(flyNum);
     }
     br.close();
@@ -67,6 +70,7 @@ public class Main {
     System.out.println(sb.toString());
   }
 
+  // h의 높이에서 돌을 던져 깨지는 미네랄 삭제하는 함수
   static void throwStone(int h, int chang) {
     int c = -1;
     if (chang == 1) {
@@ -78,7 +82,9 @@ public class Main {
         if (map[h][cc] == 'x') c = cc;
       }
     }
+    // c == -1 이라면 돌을 던져 허공을 가르는 경우
     if (c == -1) return;
+    // 그렇지 않다면 깨진 미네랄 삭제
     map[h][c] = '.';
   }
 
@@ -86,6 +92,7 @@ public class Main {
     return (0 <= r && r < R && 0 <= c && c < C);
   }
 
+  // map에서 미네랄을 BFS 탐색하여 공중에 있는 미네랄 번호를 리턴
   static int flyCheckBFS() {
     int num = 1;
     LinkedList<Node> queue = new LinkedList<>();
@@ -93,6 +100,7 @@ public class Main {
     for (int r = 0; r < R; r++) {
       for (int c = 0; c < C; c++) {
         if (map[r][c] == '.' || visited[r][c] != 0) continue;
+        // isFall: 미네랄이 공중에 떠있는지 저장하는 boolean
         boolean isFall = true;
         queue.add(new Node(r, c));
         visited[r][c] = num;
@@ -104,10 +112,12 @@ public class Main {
             if (inMap(nr, nc) && map[nr][nc] == 'x' && visited[nr][nc] == 0) {
               visited[nr][nc] = num;
               queue.add(new Node(nr, nc));
+              // 미네랄 덩이가 바닥에 붙어있을 경우 isFall = false
               if (nr == R - 1) isFall = false;
             }
           }
         }
+        // 미네랄이 공중에 떠있을 경우 해당 미네랄 번호 리턴
         if (isFall) return num;
         num++;
       }
@@ -115,6 +125,7 @@ public class Main {
     return 0;
   }
 
+  // num 번호에 해당하는 미네랄 fd 만큼 하강
   static void fall(int num) {
     int fd = fallDistance(num);
     for (int r = R - 1; r >= 0; r--) {
@@ -126,7 +137,8 @@ public class Main {
       }
     }
   }
-
+  
+  // num 번호에 해당하는 미네랄이 얼마만큼 하강할 수 있는지 거리를 구해주는 함수
   static int fallDistance(int num) {
     int fd = Integer.MAX_VALUE;
     for (int c = 0; c < C; c++) {
